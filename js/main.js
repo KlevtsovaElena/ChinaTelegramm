c113a5ed50b3c7169ade76a135b1feea3cfbe5b6
chemezoida <evchemez@mail.ru>
2022-12-18 12:58:14 +0300

    jghfhj

---
+++ js/main.js
@@ -0,0 +1,45 @@
+    let userName = localStorage.getItem('username');
+    if(userName == null || userName == ''){
+        setUsername();
+    } 
+console.log(userName);
+    function setUsername(){
+        let username = prompt("Введите Ваш никнейм:");
+        localStorage.setItem('username', username);
+    }
+
+    function addPost(){
+        let message = document.getElementById('usertext').value;
+        let date = new Date().toLocaleString();
+        let url = 'https://ChinaTelegram.perfectpink.repl.co/?addpost&name=' + userName + '&message=' + message + '&date=' + date;
+        alert(url);
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send();
+    }
+
+    function renderposts(){
+        let url = 'https://ChinaTelegram.perfectpink.repl.co/?getposts';
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send()
+        let response = xhr.responseText;
+        let db = JSON.parse(response);
+        let container = document.getElementById('message');
+        let template = document.getElementById('text-message').innerHTML;
+        container.innerHTML = "";
+        for(let i = 0; i < db.length; i++){
+            if(window.localStorage.username == db[i]['name']){
+                container.innerHTML += template.replace('${name}',db[i]['name'])
+                .replace('${text}',db[i]['message'])
+                .replace('${time}',db[i]['data'])
+                .replace('${class}','right')
+            }else{
+                container.innerHTML += template.replace('',)
+                .replace('${name}',db[i]['name'])
+                .replace('${text}',db[i]['message'])
+                .replace('${time}',db[i]['data'])
+                .replace('${class}','left')
+            }
+        }
+    }
